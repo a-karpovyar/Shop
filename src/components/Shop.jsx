3027701b140015ce4429377866e0e5603a773ffd
Anatoly Karpov <tolick.karpow@mail.ru>
2025-02-07 16:48:57 +0300

    add basket

--- src/components/Shop.jsx
+++ src/components/Shop.jsx
@@ -1,25 +1,58 @@
-import React, { useState, useEffect } from 'react';
-import { API_KEY, API_URL } from '../config';
-import { Preloader } from './Preloader';
+import React, { useState, useEffect } from "react";
+import { API_URL, API_KEY } from "../config";
+
+import { Preloader } from "./Preloader";
+import { GoodsList } from "./GoodsList";
+import { Cart } from "./Cart";
 
 function Shop() {
-    const [goods, setGoods] = useState([]);
-    const [loading, setLoading] = useState(true);
+  const [goods, setGoods] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [order, setOrder] = useState([]);
 
-    useEffect(function getGoods() {
-        fetch(API_URL, {
-            headers: {
-                'Authorization': API_KEY
-            },
-        }).then(responce => responce.json()).then(data => {
-            data.featured && setGoods(data.featured);
-            setLoading(false);
-        })
-    }, [])
+  const addToBasket = (item) => {
+    const itemIndex = order.findIndex((orderItem) => orderItem.id === item.id);
 
-    return <main className="container content">
+    if (itemIndex < 0) {
+      const newItem = {
+        ...item,
+        quantity: 1,
+      };
+      setOrder([...order, newItem]);
+    } else {
+      const newOrder = order.map((orderItem, index) => {
+        if (itemIndex === index) {
+          return {
+            ...orderItem,
+            quantity: orderItem.quantity + 1,
+          };
+        } else {
+          return orderItem;
+        }
+      });
+      console.log(newOrder);
+      setOrder(newOrder);
+    }
+  };
 
-        {loading ? <Preloader /> : goods}
+  useEffect(function getGoods() {
+    fetch(API_URL, {
+      headers: {
+        Authorization: API_KEY,
+      },
+    })
+      .then((responce) => responce.json())
+      .then((data) => {
+        data.shop && setGoods(data.shop.splice(100));
+        setLoading(false);
+      });
+  }, []);
+
+  return (
+    <main className="container content">
+      <Cart quantity={order.length} />
+      {loading ? <Preloader /> : <GoodsList goods={goods}  addToBasket={addToBasket}/>}
     </main>
-};
+  );
+}
 export { Shop };
